af970de0583efa368e98e023f843ef4fef33d2d2
Tim F <tim.fanselow@gmail.com>
2018-06-17 17:56:14 +1200

    Switched to using quad. encoder. System is basically functional

--- keyFobBassIRSensors/keyFobBassIRSensors.ino
+++ keyFobBassIRSensors/keyFobBassIRSensors.ino
@@ -1,27 +1,23 @@
-#include <TimerOne.h>
 #include <Encoder.h>
+
+#include <TimerOne.h>
 #include "QuickStats.h"
 
 const int ledPin = 13;
 const int midichannel = 1;
 
 //pluck sensor vars
-const int pluckSensorPin = A1;
+const int pluckSensorPin = A0;
 const int pluckSensorBufferSize = 30; //at 2kHz sampling, this gives window size = 15ms
 volatile float pluckSensorVals[pluckSensorBufferSize];
 volatile int pluckSensorIndex = pluckSensorBufferSize;
-const float pluckSensorLoudnessThreshold = 50;
+const float pluckSensorStdDevThreshold = 2;
 unsigned long lastPluckTime = 0;
 const int pluckDebounceTime = 50; //milliseconds
 bool lastPluckState = false; //for rising edge detection
 
+int pluckCount = 0; //for generating a tune from a single note
 
-Encoder posEncoder(5, 6);
-int encoderCorrection = 0; //used to keep encoder values positive
-
-int numNotes = 7;
-int notes[] = {24, 26, 28, 29, 31, 33, 35}; //midi codes for c major scale
-int currentNote = notes[0];
 
 QuickStats stats;
 
@@ -45,9 +41,7 @@ bool pluckDetected(){
   //compute std.dev of current window.
   //if std.dev of window is above a threshold, return true
   noInterrupts();
-  float loudness = stats.stdev((float*) pluckSensorVals, pluckSensorBufferSize);
-  //Serial.println(loudness);
-  bool plucked = loudness > pluckSensorLoudnessThreshold;
+  bool plucked = stats.stdev((float*) pluckSensorVals, pluckSensorBufferSize) > pluckSensorStdDevThreshold;
   interrupts();
 
   //debouncing
@@ -64,25 +58,6 @@ void readSensors(){
   //Serial.println(String(micros()) + ", " + String(pluckSensorVals[pluckSensorIndex]) + ",");
   //Serial.println(pluckSensorVals[pluckSensorIndex]);
 }
-
-int encoderToNote(int encoderVal){
-  //convert encoder value to a midi note
-  
-  int ticksPerNote = 40; 
-
-//  //convert to a scale
-//  int noteIndex = max(encoderVal / ticksPerNote, 0);
-//  int octaveAddition = (int(noteIndex) / numNotes) * 12; 
-//  Serial.println(octaveAddition);
-//  return notes[noteIndex % numNotes] + octaveAddition;
-
-  //alernative - just map directly to midi notes
-
-  int bassNote = 24; //middle c?
-  return encoderVal / ticksPerNote + bassNote;
-}
-
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -102,33 +77,16 @@ void loop() {
   bool pluckState = pluckDetected();
   bool pluckRisingEdge = !lastPluckState && pluckState;
   if (pluckRisingEdge){
-
-      int encoderPos = posEncoder.read();
-      int correctedPos = encoderPos + encoderCorrection;
-    
-      //if encoder value goes negative, update correction value to keep the result positive
-      if (correctedPos < 0){
-        encoderCorrection -= correctedPos;
-        correctedPos = 0;  
-      }
-      
-      int nextNote = encoderToNote(correctedPos);
-      usbMIDI.sendNoteOff(currentNote, 0, midichannel);
-      currentNote = nextNote;
-      usbMIDI.sendNoteOn(currentNote, 99, midichannel);
-      Serial.println(String("Current note: ") + String(currentNote));
+    switch (pluckCount) {
+      case 0:
+        usbMIDI.sendNoteOff(25, 0, midichannel);  
+        usbMIDI.sendNoteOn(24, 99, midichannel);  
+      break;
+      default:
+        usbMIDI.sendNoteOff(24, 0, midichannel);
+        usbMIDI.sendNoteOn(25, 99, midichannel);   // 61 = C#4
+    }
+    pluckCount = (pluckCount + 1) % 2;
   }
   lastPluckState = pluckState;
-
-
-
-//  int nextNote = encoderToNote(correctedPos);
-//  if (nextNote != currentNote){
-//    usbMIDI.sendNoteOff(currentNote, 0, midichannel);
-//    currentNote = nextNote;
-//    usbMIDI.sendNoteOn(currentNote, 99, midichannel);
-//    Serial.println(String("Current note: ") + String(currentNote));
-//  }
-  
 }
-
